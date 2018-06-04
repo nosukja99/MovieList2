75a81d12305bcb983123b6923f35ef9d898921bd
nosukja99 <nosukja99@gmail.com>
2018-06-04 16:07:13 -0400

    updated with BufferedReader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,23 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String movieTitle;
         ArrayList<String> movieTitles = movieTitles = new ArrayList<>();
+        BufferedReader bfr= new BufferedReader(new InputStreamReader(System.in));
         boolean answer;
         String inAnswer;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter your favorite movie name.");
-        movieTitle = scanner.next();
+        movieTitle = bfr.readLine();
         movieTitles.add(movieTitle);
 
         System.out.println("Do you want add more movie? <Y/y> Or not input <Q/q>");
@@ -27,7 +31,7 @@ public class Main {
         while(answer==true)
         {
             System.out.println("Please enter your favorite movie name.");
-            movieTitle = scanner.next();
+            movieTitle = bfr.readLine();
             boolean result;
             result= Contains(movieTitles, movieTitle);
             if (result == false)
